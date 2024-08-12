8705d76b8876da1c3d3f0b946d33cce0314ee096
danpham04 <phamdan2804@gmail.com>
2024-08-12 21:02:23 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_review/provider/provider_connectivity.dart';
+import 'package:flutter_review/provider/provider_create.dart';
+import 'package:flutter_review/provider/provider_home.dart';
+import 'package:flutter_review/provider/provider_update.dart';
 import 'package:flutter_review/screens/app/my_app.dart';
-
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ProviderHome(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => ProviderUpdate(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => ProviderCreate(),
+        ),
+        ChangeNotifierProvider(
+          create: (context) => ProviderConnectivity(),
+        )
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
